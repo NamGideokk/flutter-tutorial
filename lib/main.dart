f83ba48ecb5771f5b8ac5052d0b75d1205e29db2
Nam Gideok <namgd1222@gmail.com>
2022-08-31 23:09:58 +0900

    flutter 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
+  print(dotenv.env["testID"] == "");
+  print('testPW : ${dotenv.env["testPW"]}');
   runApp(const MyApp());
 }
 
@@ -10,9 +15,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Container(width: 50, height: 50, color: Colors.deepOrangeAccent,),
-      )
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Contact App"),
+          titleTextStyle: TextStyle(color: Colors.black),
+          // backgroundColor: Colors.white,
+          actions: const [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Icon(
+                Icons.add_box_outlined,
+              ),
+            )
+          ],
+          actionsIconTheme: IconThemeData(color: Colors.black),
+        ),
+        body: ListView(),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: '홈',
+                activeIcon: Icon(Icons.home_filled)),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shop),
+                label: '샵',
+                activeIcon: Icon(Icons.shop_2_outlined)),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: '검색',
+                activeIcon: Icon(Icons.search_off),
+                backgroundColor: Colors.red),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: '내정보',
+                activeIcon: Icon(Icons.person_outline)),
+          ],
+        ),
+      ),
     );
   }
 }
